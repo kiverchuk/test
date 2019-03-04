8579fc5a08ce1f8098ab4effe31f6a071fff2534
kiverchuk <32567219+kiverchuk@users.noreply.github.com>
2019-03-04 09:00:52 +0200

    ex 5

--- js/Worker.js
+++ js/Worker.js
@@ -1,13 +1,55 @@
-class Worker{
-    
+// class Worker{    
+//     constructor(name, surname, hourRate, hours){
+//         this.name = name;
+//         this.surname = surname;
+//         this.hourRate = hourRate;
+//         this.hours = hours;
+//     }
+//     getSalary(){
+//         return this.hourRate*this.hours
+//     }
+// }
+
+class Worker{ 
     constructor(name, surname, hourRate, hours){
         this.name = name;
         this.surname = surname;
         this.hourRate = hourRate;
         this.hours = hours;
     }
-
     getSalary(){
         return this.hourRate*this.hours
     }
-};
+
+    increaseSalary(i){
+        this.hourRate += i;
+    }
+
+    getName(){
+        return this.name;
+    }
+    setName(name){
+        this.surname = name;
+    }
+
+    getSurname(){
+        return this.surname;
+    }
+    setSurname(surname){
+        this.surname = surname;
+    }
+
+    getRate(){
+        return this.hourRate;
+    }
+    setRate(rate){
+        this.hourRate = rate;
+    }
+
+    getHours(){
+        return this.hours;
+    }
+    setHours(hours){
+        this.hours = hours;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
-let worker1 = new Worker("Maria", 'Lisevici', 12, 160); 
-console.log(worker1.name); 
-console.log(worker1.surname);
-console.log(worker1.hourRate);
-console.log(worker1.hours); 
-console.log(worker1.getSalary()); 
+// let worker1 = new Worker("Maria", 'Lisevici', 12, 160); 
+// console.log(worker1.name); 
+// console.log(worker1.surname);
+// console.log(worker1.hourRate);
+// console.log(worker1.hours); 
+// console.log(worker1.getSalary()); 
 
-let worker2 = new Worker("Mihai", 'Crihan', 8, 200); 
-console.log(worker2.name);
-console.log(worker2.surname);
-console.log(worker2.hourRate); 
-console.log(worker2.hours); 
-console.log(worker2.getSalary());
+// let worker2 = new Worker("Mihai", 'Crihan', 8, 200); 
+// console.log(worker2.name);
+// console.log(worker2.surname);
+// console.log(worker2.hourRate); 
+// console.log(worker2.hours); 
+// console.log(worker2.getSalary());
 
-console.log((worker1.getSalary()>worker2.getSalary()?
-    worker1.name+" salariu mai mare cu: "+ (worker1.getSalary()-worker2.getSalary()):
-    worker2.name+" salariu mai mare cu: "+ (worker2.getSalary()-worker1.getSalary())
-))
+// console.log((worker1.getSalary()>worker2.getSalary()?
+//     worker1.name+" salariu mai mare cu: "+ (worker1.getSalary()-worker2.getSalary()):
+//     worker2.name+" salariu mai mare cu: "+ (worker2.getSalary()-worker1.getSalary())
+// ))
+
+var worker = new Worker("Ion", 'Creangă', 10, 176); console.log(worker.getName()); // afișează Ion 
+console.log(worker.getSurname()); // afișează Creangă 
+console.log(worker.getRate()); // afișează 10 
+console.log(worker.getHours()); // afișează 176 
+console.log(worker.getSalary()); //afișează 1760 = 10*176
+worker.increaseSalary(2); //mărește hourRate cu 2
+console.log(worker.getSalary()); //afișează 2112 = 12*176
